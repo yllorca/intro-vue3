137cba47ea27948efd7852e7e083361c3337a340
yllorca <yllorca@helloworld.cl>
2022-11-23 15:23:41 -0300

    Add descripcion for product. Coding Challenge L2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Lamp'
+            product: 'Lamp',
+            description: 'this a description for producto'
         }
     }
 })
