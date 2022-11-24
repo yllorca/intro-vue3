14d8cbfbaefdf1aa3373d40e792c56e117909bf1
yllorca <yllorca@helloworld.cl>
2022-11-24 12:15:21 -0300

    challange code solved

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 1,
+            onSale: false,
         }
     }
 })
