ac77818de3406d8126102ce901715f74448a824a
yllorca <yllorca@helloworld.cl>
2022-12-15 22:07:31 -0300

    End L10-start

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+      UpdateCart(id) {
+        this.cart.push(id)
+      }
+    }
 })
